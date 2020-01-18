5a7e755d16b05b63903c98bfe9255ddb27342b7d
Paul Falkenberg <paulfalkenberg23@gmail.com>
2020-01-17 19:10:12 -0500

    Fixed Id property being ignored when being mapped from domain object to DTO

--- App_Start/MappingProfile.cs
+++ App_Start/MappingProfile.cs
@@ -12,10 +12,10 @@ namespace VideoStoreManager.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDTO>().ForMember(c => c.Id, opt => opt.Ignore());
-            Mapper.CreateMap<CustomerDTO, Customer>();
-            Mapper.CreateMap<Movie, MovieDTO>().ForMember(m => m.Id, opt => opt.Ignore());
-            Mapper.CreateMap<MovieDTO, Movie>();
+            Mapper.CreateMap<Customer, CustomerDTO>();
+            Mapper.CreateMap<CustomerDTO, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Movie, MovieDTO>();
+            Mapper.CreateMap<MovieDTO, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
 
     }
